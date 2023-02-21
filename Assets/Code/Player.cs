3c6c298370cf955752480d86af8d88af1bf2b116
Ahnaf Kazi <33816349+AhnafK@users.noreply.github.com>
2023-02-21 04:20:25 -0500

    Basic shots and variables (they are sideways oopsies)

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -6,11 +6,16 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     Rigidbody2D _rigidbody;
+    public int bullets;
+    public float health = 100;
+    public float hunger = 100;
+    public GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        bullets = 1;
     }
 
     // Update is called once per frame
@@ -26,11 +31,19 @@ public class Player : MonoBehaviour
             if (context.ReadValue<float>() > 0)
             {
                 Debug.Log("Scrolling up");
+                if (bullets < 5)
+                {
+                    bullets++;
+                }
             }
             else if (context.ReadValue<float>() < 0)
             {
                 Debug.Log("Scrolling down");
-        }
+                if (bullets > 1)
+                {
+                    bullets--;
+                }
+            }
         }
     }
 
@@ -39,6 +52,21 @@ public class Player : MonoBehaviour
     {
         Vector2 move = context.ReadValue<Vector2>();
         _rigidbody.velocity = move * 5;
+    }
+
+    // shoot towards mouse
+    public void OnShoot(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            for (int i = 0; i < bullets; i++)
+            {
+                GameObject firedBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+                firedBullet.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()).y - transform.position.y, Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()).x - transform.position.x) * Mathf.Rad2Deg);
+                firedBullet.transform.localScale *= bullets;
+                firedBullet.GetComponent<Rigidbody2D>().velocity = (Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - transform.position).normalized * 10;
+            }
+        }
     }
 
 }
