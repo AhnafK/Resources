4cdd8193b0c18899dc96b27606ae62ed36193985
Ahnaf Kazi <33816349+AhnafK@users.noreply.github.com>
2023-02-22 04:18:22 -0500

    Added some stuff bullets destroy for now

--- Assets/Code/Bullet.cs
+++ Assets/Code/Bullet.cs
@@ -7,12 +7,21 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 5);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -2,26 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     Rigidbody2D _rigidbody;
-    public int bullets;
+    public int bullets = 1;
+    public float distForAmmo = 2;
+    public float distForHunger = 1;
+    public int bulletSpeed = 10;
+    public int ammo = 0;
     public float health = 100;
+    float maxHealth;
     public float hunger = 100;
+    float maxHunger;
+    public float initialSpeed = 5;
+    float speed;
     public GameObject bulletPrefab;
+    public Text ammoText;
+    public Text healthText;
+    public Text hungerText;
+    public Transform hungerBar;
+    
 
-    // Start is called before the first frame update
+    Vector2 lastPos;
+    float distance;
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        speed = initialSpeed;
         bullets = 1;
+        maxHealth = health;
+        maxHunger = hunger;
+        lastPos = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-       
+        // reduce hunger and gain ammo based on distance moved
+        distance += Vector2.Distance(transform.position, lastPos);
+        hunger -= Vector2.Distance(transform.position, lastPos) / distForHunger;
+        if (hunger <= 0)
+        {
+            health += hunger/10;
+            hunger = 0;
+            speed = initialSpeed / 5;
+        }
+        else
+        {
+            speed = initialSpeed;
+        }
+        if (hunger > maxHunger)
+        {
+            health += (hunger - maxHunger) / 3;
+            hunger = maxHunger;
+        }
+        if (health > maxHealth)
+        {
+            health = maxHealth;
+        }
+        else if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+        if (distance >= distForAmmo)
+        {
+            ammo += (int)(distance / distForAmmo);
+            distance = distance % distForAmmo;
+        }
+        lastPos = transform.position;
+        hungerText.text = ""+hunger;
+        healthText.text = ""+health;
+        ammoText.text = ""+ammo;
+        hungerBar.localScale = new Vector3(hunger/10, hungerBar.localScale.y, hungerBar.localScale.z);
     }
 
     // Called when player scrolls the wheel on the mouse
@@ -51,21 +105,21 @@ public class Player : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         Vector2 move = context.ReadValue<Vector2>();
-        _rigidbody.velocity = move * 5;
+        _rigidbody.velocity = move * speed;
     }
 
     // shoot towards mouse
     public void OnShoot(InputAction.CallbackContext context)
     {
-        if (context.started)
-        {
-            for (int i = 0; i < bullets; i++)
-            {
-                GameObject firedBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-                firedBullet.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()).y - transform.position.y, Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()).x - transform.position.x) * Mathf.Rad2Deg);
-                firedBullet.transform.localScale *= bullets;
-                firedBullet.GetComponent<Rigidbody2D>().velocity = (Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()) - transform.position).normalized * 10;
-            }
+        if (context.started && ammo >= bullets)
+        {            
+            GameObject firedBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            Vector2 currPos = transform.position;
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            firedBullet.transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(mousePos.y - currPos.y, mousePos.x - currPos.x) * Mathf.Rad2Deg + 90);            
+            firedBullet.GetComponent<Rigidbody2D>().velocity = (mousePos - currPos).normalized * bulletSpeed;
+            print(firedBullet.GetComponent<Rigidbody2D>().velocity.magnitude);           
+            ammo -= bullets;
         }
     }
 
